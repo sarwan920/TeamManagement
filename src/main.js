6bab72344b38a685d73c3eef23108901fd91f472
Sarwan <sarwan.nizamani8@gmail.com>
2020-12-11 03:24:15 -0800

    add dynamic routing

--- src/main.js
+++ src/main.js
@@ -3,16 +3,19 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import UsersList from './components/users/UsersList.vue'
 import TeamsList from './components/teams/TeamsList.vue'
+import TeamMembers from './components/teams/TeamMembers.vue'
 
 import App from './App.vue';
 
-const router= createRouter({
-    history:createWebHistory(),
-    routes:[
-        { path:'/users' , component:UsersList},
-        {path:'/teams',component:TeamsList}
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/users', component: UsersList },
+        { path: '/teams', component: TeamsList },
+        {path:'/teams/:teamId', component: TeamMembers}
     ],
-    linkActiveClass:'active'
+    linkActiveClass: 'active',
+
 });
 
 
